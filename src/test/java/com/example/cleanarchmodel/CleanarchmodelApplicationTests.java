8a1e604700284f56be740969b66a33ddf50b5328
Guilsimi <guilherme.simioni04@gmail.com>
2025-11-13 11:12:24 -0400

    initial commit

---
+++ src/test/java/com/example/cleanarchmodel/CleanarchmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
