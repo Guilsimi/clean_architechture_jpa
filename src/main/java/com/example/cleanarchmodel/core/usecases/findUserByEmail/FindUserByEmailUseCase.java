356dcae647d0828d344669fc389453f72e10c9ca
Guilsimi <guilherme.simioni04@gmail.com>
2025-11-13 11:46:19 -0400

    cleanup: remove user import in findUserByEmailUseCase

--- src/main/java/com/example/cleanarchmodel/core/usecases/findUserByEmail/FindUserByEmailUseCase.java
+++ src/main/java/com/example/cleanarchmodel/core/usecases/findUserByEmail/FindUserByEmailUseCase.java
@@ -1,6 +1,5 @@
 package com.example.cleanarchmodel.core.usecases.findUserByEmail;
 
-import com.example.cleanarchmodel.core.domain.user.User;
 import com.example.cleanarchmodel.core.domain.user.UserResponseDTO;
 
 public interface FindUserByEmailUseCase {
